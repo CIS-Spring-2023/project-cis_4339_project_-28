a94279bf745e4a9f936f830a867e3ef568b8e382
wakindo <70498088+wakindo@users.noreply.github.com>
2023-04-14 15:36:50 -0500

    Moved Client Chart to Home Page
    As per reqs

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -170,42 +170,26 @@ export default {
             </tr>
           </tbody>
         </table>
-        <br />
-        <div class="mt-40">
-          <h2 class="italic font-bold text-center">
-            Chart of Clients by Zip Code below:
-          </h2>
+        <!-- Start of loading animation -->
+        <div class="mt-40" v-if="loading">
+          <p
+            class="text-6xl font-bold text-center text-gray-500 animate-pulse"
+          >
+            Loading...
+          </p>
         </div>
-        <br />
-        <!-- @wakindo: Clients Chart stuff starts here   -->
-        <div>
-          <clientChart
-            v-if="!loading && !error"
-            :label="labels"
-            :chart-data="chartData"
-          ></clientChart>
+        <!-- End of loading animation -->
 
-          <!-- Start of loading animation -->
-          <div class="mt-40" v-if="loading">
-            <p
-              class="text-6xl font-bold text-center text-gray-500 animate-pulse"
-            >
-              Loading...
-            </p>
-          </div>
-          <!-- End of loading animation -->
-
-          <!-- Start of error alert -->
-          <div class="mt-12 bg-red-50" v-if="error">
-            <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
-              {{ error.title }}
-            </h3>
-            <p class="p-4 text-lg font-bold text-red-900">
-              {{ error.message }}
-            </p>
-          </div>
-          <!-- End of error alert -->
+        <!-- Start of error alert -->
+        <div class="mt-12 bg-red-50" v-if="error">
+          <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
+            {{ error.title }}
+          </h3>
+          <p class="p-4 text-lg font-bold text-red-900">
+            {{ error.message }}
+          </p>
         </div>
+        <!-- End of error alert -->
       </div>
     </div>
   </main>
